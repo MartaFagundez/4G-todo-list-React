a73dec3ea42280a29e9a0e83fc1d5230d78d6b8d
MartaFagundez <55920614+MartaFagundez@users.noreply.github.com>
2023-11-10 21:54:52 -0300

    Project done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,55 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import Task from "./task";
 
 //create your first component
 const Home = () => {
+	const [newTodo, setNewTodo] = useState("");
+	const [todos, setTodos] = useState([]);
+
+	function addTodo(e) {
+		if (e.key === "Enter") {
+			setTodos([...todos, newTodo]);
+			setNewTodo("");
+		}
+	}
+
+	function deleteTodo(i) {
+		setTodos(todos.filter((todo, index) => index !== i));
+	}
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="d-flex flex-column justify-content-center align-items-center min-vh-100" style={{backgroundColor: "#f5f5f5", paddingTop: "12vh"}} >
+			<h1 className="colorPurple display-1 fw-light mb-4" style={{marginTop: "-24vh"}} >todos</h1>
+			
+			<div className="d-flex flex-column w-100 shadow" style={{maxWidth: "400px"}}>
+				<input
+					type="text"
+					className="text-muted fw-light fs-4 py-2 px-4 d-block w-100"
+					style={{"--bs-text-opacity": 0.25}}
+					placeholder="Enter your todo"
+					autoComplete="off"
+					value={newTodo}
+					onChange={e => setNewTodo(e.target.value)}
+					onKeyDown={addTodo}
+				/>
+
+				<ul className="bg-white m-0 list-unstyled">
+					{todos.map((todo, index) => {
+						return (
+							<Task key={index} id={index} task={todo} deleteHandler={deleteTodo}></Task>
+						)
+					})}
+				</ul>
+
+				<p className="text-black-50 fw-light fs-6 m-0 px-3 py-2 d-block w-100">
+					{
+						todos.length > 1 ? `${todos.length} items left` 
+						: todos.length === 1 ? "1 item left" 
+						: "No tasks, add a task"
+					}
+				</p>
+			</div>
 		</div>
 	);
 };
---
+++ src/js/component/task.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+export default function Task({id, task, deleteHandler}) {
+    const [isHover, setIsHover] = useState(false);
+
+  return (
+    <div className="d-flex align-items-center position-relative"
+        onMouseEnter={() => setIsHover(true)}
+        onMouseLeave={() => setIsHover(false)}
+    >
+        <li className={`fw-light fs-4 py-2 px-4 d-flex w-100 border-bottom ${isHover ? "colorGray" : "colorLightGray"}`}>
+            {task}
+        </li>
+
+        <button 
+            onClick={() => deleteHandler(id)} 
+            className={`btn colorLightGray fs-6 position-absolute top-10 end-0 ${isHover ? "d-block" : "d-none"}`}
+        >
+            x
+        </button>
+    </div>
+  )
+}
+
